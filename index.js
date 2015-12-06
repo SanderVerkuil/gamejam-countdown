ec57fb581bebdd520c88a9c38820d25fdd67200a
Sander Verkuil <saverkuil@gmail.com>
2015-12-06 22:32:06 +0100

    Changed the display, and fixed minor issues.

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 })
 
+app.get('/reset', function(req, res) {
+  console.log(playing);
+  playing = false;
+  res.sendFile(__dirname + '/index.html');
+});
+
 app.use('/public', express.static(__dirname + '/public'));
 
 io.on('connection', function(socket){
@@ -24,19 +30,30 @@ var playing = false;
 setInterval(function() {
   var options = YAML.load("options.yml");
 
-  var target = moment(options.options.time.end);
-  var start = moment(options.options.time.start)
+  var target = moment(options.time.end);
+  var start = moment(options.time.start)
   var totalTime = target.diff(start);
 
   var now = moment();
   var diff = target.diff(now);
-  if (!playing && diff < options.options.song.start)
-  {
-    player.play(options.options.song.location)
-    playing = true;
-  }
+  var sdiff = start.diff(now);
+  console.log(sdiff);
   if (start.diff(now) < 0)
   {
+    if (diff < options.song.start)
+    {
+      if (!playing) {
+        player.play(options.song.location);
+        console.log("Started to play at " + now.format("YYYY-MM-DD, HH:mm:ss"));
+        playing = true;
+      }
+      console.log(diff);
+    } else {
+      if (playing) {
+        console.log("Started to play at " + now.format("YYYY-MM-DD, HH:mm:ss"));
+        playing = false;
+      }
+    }
     if (diff > 0)
       io.emit('tick', {start: now, end: target, percentage: diff / totalTime, complete: false, started: true});
     else {
@@ -45,7 +62,7 @@ setInterval(function() {
   } else {
     io.emit('tick', {start: start, end: target, percentage: 100, complete: false, started: false});
   }
-}, 1000)
+}, 990)
 
 http.listen(3000, function() {
   console.log('listening on *:3000');
