dff9ffcf8afbf04d9a868ed8476c1e64552fd72e
Sander Verkuil <saverkuil@gmail.com>
2015-12-06 21:11:18 +0100

    Finished the project.

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ var moment = require('moment');
 var player = require('play-sound')(opts={});
 var YAML = require('yamljs');
 
-var options = YAML.load("options.yml");
-console.log(options);
-
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 })
@@ -22,13 +19,15 @@ io.on('connection', function(socket){
   })
 })
 
-var target = moment(options.options.time.end);
-var start = moment(options.options.time.start)
-var totalTime = target.diff(start);
-
 var playing = false;
 
 setInterval(function() {
+  var options = YAML.load("options.yml");
+
+  var target = moment(options.options.time.end);
+  var start = moment(options.options.time.start)
+  var totalTime = target.diff(start);
+
   var now = moment();
   var diff = target.diff(now);
   if (!playing && diff < options.options.song.start)
@@ -36,10 +35,15 @@ setInterval(function() {
     player.play(options.options.song.location)
     playing = true;
   }
-  if (diff > 0)
-    io.emit('tick', {start: now, end: target, percentage: diff / totalTime, complete: false});
-  else {
-    io.emit('tick', {start: target, end: target, percentage: 0, complete: true});
+  if (start.diff(now) < 0)
+  {
+    if (diff > 0)
+      io.emit('tick', {start: now, end: target, percentage: diff / totalTime, complete: false, started: true});
+    else {
+      io.emit('tick', {start: target, end: target, percentage: 0, complete: true, started: true});
+    }
+  } else {
+    io.emit('tick', {start: start, end: target, percentage: 100, complete: false, started: false});
   }
 }, 1000)
 
